5125dfc903e41cbbd26932bf99d83a9712c715d7
nguyenreal <ntnn51@gmail.com>
2024-10-21 11:26:29 +0700

    Test

--- FengShuiKoi/FengShuiKoi/obj/Debug/net8.0-windows/FengShuiKoi.AssemblyInfo.cs
+++ FengShuiKoi/FengShuiKoi/obj/Debug/net8.0-windows/FengShuiKoi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FengShuiKoi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b89a338f0a80e25b7370565a854b229752f50ffe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+804f2736fcb0109fbc07a3a4be04a48aaefbb6ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("FengShuiKoi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FengShuiKoi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
