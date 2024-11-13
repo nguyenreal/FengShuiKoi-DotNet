81295d24d870e3e0b289c0836a0167ad6a06cc9e
nguyenreal <ntnn51@gmail.com>
2024-11-13 19:50:09 +0700

    Merge NguyenNTN

--- FengShuiKoi/FengShuiKoi/obj/Debug/net8.0-windows/FengShuiKoi.AssemblyInfo.cs
+++ FengShuiKoi/FengShuiKoi/obj/Debug/net8.0-windows/FengShuiKoi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FengShuiKoi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9db41c08d878b54c2e4d6290fe9418511d10e316")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a49abc36d30478f3b75f7b7832b6739b354be0f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FengShuiKoi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FengShuiKoi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
